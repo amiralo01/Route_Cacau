f49e5cb868c681f2910c15b0b7b275c86a895f8c
Amiralo01 <amiraldo.f19@gmail.com>
2023-11-29 14:23:55 -0300

    firebase integração

--- LEAFLET/firebase.js
+++ LEAFLET/firebase.js
@@ -1,22 +1,22 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+//import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-apiKey: "AIzaSyAoqsteBxJxWIItJ5D1XyHz8mbDq3Mr0Qg",
-authDomain: "routecacau.firebaseapp.com",
-databaseURL: "https://routecacau-default-rtdb.firebaseio.com",
-projectId: "routecacau",
-storageBucket: "routecacau.appspot.com",
-messagingSenderId: "585882747400",
-appId: "1:585882747400:web:d3e23d334dd3a8fe9487bd",
-measurementId: "G-X6M6TNKQ0C"
+var firebaseConfig = {
+    apiKey: "AIzaSyAoqsteBxJxWIItJ5D1XyHz8mbDq3Mr0Qg",
+    authDomain: "routecacau.firebaseapp.com",
+    databaseURL: "https://routecacau-default-rtdb.firebaseio.com",
+    projectId: "routecacau",
+    storageBucket: "routecacau.appspot.com",
+    messagingSenderId: "585882747400",
+    appId: "1:585882747400:web:d3e23d334dd3a8fe9487bd",
+    measurementId: "G-X6M6TNKQ0C"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
