48d6c8834fd33438e3de50e4cd38c0acf2880ee3
Jarlison Neves <88516006+Jarlisonn@users.noreply.github.com>
2023-11-30 20:17:25 -0300

    Add files via upload

--- src/models/fornecedor.model.ts
+++ src/models/fornecedor.model.ts
@@ -1,4 +1,4 @@
-import {Entity, model, property, hasMany} from '@loopback/repository';
+import {Entity, hasMany, model, property} from '@loopback/repository';
 import {Localidade} from './localidade.model';
 
 @model()
@@ -27,6 +27,12 @@ export class Fornecedor extends Entity {
     required: true,
   })
   senha: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  img: String;
 
   @hasMany(() => Localidade)
   Loc_Fornecedor: Localidade[];
